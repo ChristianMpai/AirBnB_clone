578e36862280d9a472e709907a1df3cbc0756326
ChristianMpai <105034854+ChristianMpai@users.noreply.github.com>
2022-10-29 13:48:50 +0200

    defines all common attributes/methods for other classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,58 +1,43 @@
-#!/usr/bin/pyhon3
-"""
-Parent class that will inherit
-"""
-import uuid
+#!/usr/bin/python3
+"""parent class that will inherit"""
+
+import models
+from uuid import uuid4
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """Defines all common attributes/methods
-    """
+    """Type class of BaseModel"""
+
     def __init__(self, *args, **kwargs):
-        """initializes all attributes
-        """
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+        """Type method initialize"""
+        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(val, timeformat))
+                elif key != '__class__':
+                    setattr(self, key, val)
         else:
-            f = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
-                if key != '__class__':
-                    setattr(self, key, value)
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
+
+    def save(self):
+        """Type method save"""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Type method to_dict"""
+        rt_dict = self.__dict__.copy()
+        rt_dict["created_at"] = self.created_at.isoformat()
+        rt_dict["updated_at"] = self.updated_at.isoformat()
+        rt_dict["__class__"] = self.__class__.__name__
+        return rt_dict
 
     def __str__(self):
-         """returns class name, id and attribute dictionary
-         """
-         class_name = "[" + self.__class__.__name__ + "]"
-         dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-         return class_name + " (" + self.id + ") " + str(dct)
-
-     def save(self):
-         """updates last update time
-         """
-         self.updated_at = datetime.now()
-         storage.save()
-
-     def to_dict(self):
-         """creates a new dictionary, adding a key and returning
-         datemtimes converted to strings
-         """
-         new_dict = {}
-
-         for key, values in self.__dict__.items():
-             if key == "created_at" or key == "updated_at":
-                 new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-             else:
-                 if not values:
-                     pass
-                 else:
-                     new_dict[key] = values
-         new_dict['__class__'] = self.__class__.__name__
-
-         return new_dict
+        """Type method __str__"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
